2c2c960f36c0d746218e9ce611f4f6b9c162c3bf
ragul-2004 <ragulsmart04@gmail.com>
2024-07-22 09:01:16 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'; 
+import TodoList from './assets/component/ToDoList/ToDoList';
+ 
+
+const App = () => {
+  return (
+    <div className="App">
+     <TodoList></TodoList>
+    </div>
+  );
+};
+
+export default App;
